4a9ea37d64eb573cb1801c5c8879faf029e6657c
Mirsaid Akbaraliyev <akbaraliyev522@mail.ru>
2024-09-24 11:24:44 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const label = document.querySelector('label');
+const  input = document.querySelectorAll('input');
+
+
+
+
+container.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+})
